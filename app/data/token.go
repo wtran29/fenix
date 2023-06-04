92eb499a45fb4a6fd549340700ba30123a4d957f
William Tran <wtran29@gmail.com>
2023-06-03 17:19:44 -0700

    quick fix on naming convention

--- app/data/token.go
+++ app/data/token.go
@@ -10,7 +10,7 @@ type Token struct {
 	PlainText string    `db:"token_hash" json:"-"`
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
-	Expires   time.Time `db:"expires" json:"expires"`
+	Expires   time.Time `db:"expiry" json:"expiry"`
 }
 
 func (t *Token) Table() string {
