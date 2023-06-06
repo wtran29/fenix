914aed1474d192b0516c02b7778e3dfa59c99f0c
William Tran <wtran29@gmail.com>
2023-06-05 17:31:50 -0700

    imported dockertest library, setup mockdb credentials, dummy user, open db conneciton

---
+++ app/data/integration_test.go
@@ -0,0 +1,80 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ory/dockertest/v3/docker"
+
+	"github.com/ory/dockertest/v3"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "secret"
+	dbName   = "fenix_test"
+	port     = "5435"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+)
+
+var dummyUser = User{
+	FirstName: "Test",
+	LastName:  "Dummy",
+	Email:     "me@here.com",
+	Active:    1,
+	Password:  "password",
+}
+
+var models Models
+var testDB *sql.DB
+var resource *dockertest.Resource
+var pool *dockertest.Pool
+
+func TestMain(m *testing.M) {
+	os.Setenv("DATABASE_TYPE", "postgres")
+
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not connect to docker: %s", err)
+		return
+	}
+
+	pool = p
+	opts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13.4",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+
+	resource, err = pool.RunWithOptions(&opts)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resource: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not connect to docker : %s", err)
+	}
+}
--- fenix/go.mod
+++ fenix/go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
