d033c4e1daab828f0a7a17cedbf139a8b917e2c3
William Tran <wtran29@gmail.com>
2023-06-08 13:41:01 -0700

    added file exists validation for model case

--- fenix/cmd/cli/make.go
+++ fenix/cmd/cli/make.go
@@ -88,6 +88,9 @@ func doMake(arg2, arg3 string) error {
 		}
 
 		fileName := fnx.RootPath + "/data/" + strings.ToLower(modelName) + ".go"
+		if fileExists(fileName) {
+			exitGracefully(errors.New(fileName + " already exists!"))
+		}
 
 		model = strings.ReplaceAll(model, "$MODELNAME$", strcase.ToCamel(modelName))
 		model = strings.ReplaceAll(model, "$TABLENAME$", tableName)
