1c2988767e5ff6982f646098a0df9f623b27e471
William Tran <wtran29@gmail.com>
2023-06-12 19:33:52 -0700

    added remember me functionality to login and logout method, handling expiry for these tokens

--- app/handlers/auth-handlers.go
+++ app/handlers/auth-handlers.go
@@ -1,6 +1,14 @@
 package handlers
 
-import "net/http"
+import (
+	"app/data"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 func (h *Handlers) UserLogin(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.Page(w, r, "login", nil, nil)
@@ -36,6 +44,43 @@ func (h *Handlers) PostUserLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// check remember me?
+	if r.Form.Get("remember") == "remember" {
+		randStr, _ := h.randomString(12)
+
+		sha := sha256.New()
+		_, err := sha.Write([]byte(randStr))
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		hash := base64.URLEncoding.EncodeToString(sha.Sum(nil))
+		rToken := data.RememberToken{}
+		err = rToken.InsertToken(user.ID, hash)
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		// set cookie - default 30 days
+		expiry := time.Now().Add(30 * 24 * time.Hour)
+		cookie := http.Cookie{
+			Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+			Value:    fmt.Sprintf("%d|%s", user.ID, hash),
+			Path:     "/",
+			Expires:  expiry,
+			HttpOnly: true,
+			Domain:   h.App.Session.Cookie.Domain,
+			MaxAge:   2592000,
+			Secure:   h.App.Session.Cookie.Secure,
+			SameSite: h.App.Session.Cookie.SameSite,
+		}
+		http.SetCookie(w, &cookie)
+		// save hash in session
+		h.App.Session.Put(r.Context(), "userID", user.ID)
+	}
+
 	h.App.Session.Put(r.Context(), "userID", user.ID)
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -43,7 +88,34 @@ func (h *Handlers) PostUserLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handlers) Logout(w http.ResponseWriter, r *http.Request) {
+	// delete remember token if it exists
+	if h.App.Session.Exists(r.Context(), "remember_token") {
+		rToken := data.RememberToken{}
+		err := rToken.Delete(h.App.Session.GetString(r.Context(), "remember_token"))
+		if err != nil {
+			log.Printf("Failed to delete remember token: %s", err)
+		}
+	}
+
+	// delete cookie
+	cookie := http.Cookie{
+		Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Now().Add(-100 * time.Hour),
+		HttpOnly: true,
+		Domain:   h.App.Session.Cookie.Domain,
+		MaxAge:   -1,
+		Secure:   h.App.Session.Cookie.Secure,
+		SameSite: h.App.Session.Cookie.SameSite,
+	}
+	http.SetCookie(w, &cookie)
+
 	h.App.Session.RenewToken(r.Context())
 	h.App.Session.Remove(r.Context(), "userID")
+	h.App.Session.Remove(r.Context(), "remember_token")
+	h.App.Session.Destroy(r.Context())
+	h.App.Session.RenewToken(r.Context())
+
 	http.Redirect(w, r, "/users/login", http.StatusSeeOther)
 }
