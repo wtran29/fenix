70dae26fc4490e778e8b428aa4b950c06af98751
William Tran <wtran29@gmail.com>
2023-06-15 16:28:13 -0700

    create general env file template for user, include generating makefile for windows or mac users

--- fenix/cmd/cli/new.go
+++ fenix/cmd/cli/new.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"runtime"
 	"strings"
 
 	"github.com/fatih/color"
@@ -24,7 +27,7 @@ func doNew(appName string) {
 	// git clone the skeleton application
 	color.Green("\tCloning repository...")
 	_, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
-		URL:      "git@github.com/wtran29/fenix-app.git",
+		URL:      "https://github.com/wtran29/fenix-app.git",
 		Progress: os.Stdout,
 		Depth:    1,
 	})
@@ -34,10 +37,67 @@ func doNew(appName string) {
 	}
 
 	// remove .git directory
+	err = os.RemoveAll(fmt.Sprintf("./%s/.git", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// create general .env file
 
+	color.Yellow("\tCreating .env file...")
+	data, err := templateFS.ReadFile("templates/env.txt")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	env := string(data)
+	env = strings.ReplaceAll(env, "${APP_NAME}", appName)
+	key, _ := fnx.RandomString(32)
+	env = strings.ReplaceAll(env, "${KEY}", key)
+
+	err = copyDataToFile([]byte(env), fmt.Sprintf("./%s/.env", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 	// create a makefile
+	if runtime.GOOS == "windows" {
+		source, err := os.Open(fmt.Sprintf("./%s/Makefile.windows", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer source.Close()
+
+		destination, err := os.Create(fmt.Sprintf("./%s/Makefile", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer destination.Close()
+
+		_, err = io.Copy(destination, source)
+		if err != nil {
+			exitGracefully(err)
+		}
+	} else {
+		source, err := os.Open(fmt.Sprintf("./%s/Makefile.mac", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer source.Close()
+
+		destination, err := os.Create(fmt.Sprintf("./%s/Makefile", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer destination.Close()
+
+		_, err = io.Copy(destination, source)
+		if err != nil {
+			exitGracefully(err)
+		}
+	}
+
+	_ = os.Remove("./" + appName + "/Makefile.mac")
+	_ = os.Remove("./" + appName + "/Makefile.windows")
 
 	// update go.mod file
 
