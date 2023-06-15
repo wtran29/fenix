ca42e9913c14f05c6251334e5b54dab3e4b388c3
William Tran <wtran29@gmail.com>
2023-06-14 20:49:04 -0700

    added functions to generate the current fenix templates

--- fenix/cmd/cli/auth.go
+++ fenix/cmd/cli/auth.go
@@ -40,6 +40,11 @@ func doAuth() error {
 		exitGracefully(err)
 	}
 
+	err = copyFileFromTemplate("templates/data/remember_token.go.txt", fnx.RootPath+"/data/remember_token.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
 	// copy middleware
 	err = copyFileFromTemplate("templates/middleware/auth.go.txt", fnx.RootPath+"/middleware/auth.go")
 	if err != nil {
@@ -47,6 +52,41 @@ func doAuth() error {
 	}
 
 	err = copyFileFromTemplate("templates/middleware/auth-token.go.txt", fnx.RootPath+"/middleware/auth-token.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/middleware/remember.go.txt", fnx.RootPath+"/middleware/remember.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/handlers/auth-handlers.go.txt", fnx.RootPath+"/handlers/auth-handlers.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/mailer/password-reset.html.tmpl", fnx.RootPath+"/mail/password-reset.html.tmpl")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/mailer/password-reset.plain.tmpl", fnx.RootPath+"/mail/password-reset.plain.tmpl")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/login.jet", fnx.RootPath+"/views/login.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/forgot.jet", fnx.RootPath+"/views/forgot.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/reset-password.jet", fnx.RootPath+"/views/reset-password.jet")
 	if err != nil {
 		exitGracefully(err)
 	}
