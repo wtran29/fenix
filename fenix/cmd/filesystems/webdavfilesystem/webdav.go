6d9cd92549fd9c3fadcaed7479573a522d977c54
William Tran <wtran29@gmail.com>
2023-06-23 11:56:59 -0700

    added crud methods specific to webdav

--- fenix/cmd/filesystems/webdavfilesystem/webdav.go
+++ fenix/cmd/filesystems/webdavfilesystem/webdav.go
@@ -2,8 +2,10 @@ package webdavfilesystem
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/studio-b12/gowebdav"
 	"github.com/wtran29/fenix/fenix/cmd/filesystems"
@@ -38,13 +40,70 @@ func (w *WebDAV) Put(filename, folder string) error {
 
 func (w *WebDAV) List(prefix string) ([]filesystems.Listing, error) {
 	var listing []filesystems.Listing
+	client := w.getCredentials()
+	files, err := client.ReadDir(prefix)
+	if err != nil {
+		return listing, err
+	}
+
+	for _, file := range files {
+		if !strings.HasPrefix(file.Name(), ".") {
+			b := float64(file.Size())
+			kb := b / 1024
+			mb := kb / 1024
+			current := filesystems.Listing{
+				LastModified: file.ModTime(),
+				Key:          file.Name(),
+				Size:         mb,
+				IsDir:        file.IsDir(),
+			}
+			listing = append(listing, current)
+		}
+	}
+
 	return listing, nil
 }
 
 func (w *WebDAV) Delete(itemsToDel []string) bool {
+	client := w.getCredentials()
+	for _, item := range itemsToDel {
+		err := client.Remove(item)
+		if err != nil {
+			return false
+		}
+	}
 	return true
 }
 
 func (w *WebDAV) Get(destination string, items ...string) error {
+	client := w.getCredentials()
+
+	for _, item := range items {
+		err := func() error {
+			webdavFilePath := item
+			localFilePath := fmt.Sprintf("%s/%s", destination, path.Base(item))
+
+			reader, err := client.ReadStream(webdavFilePath)
+			if err != nil {
+				return err
+			}
+
+			file, err := os.Create(localFilePath)
+			if err != nil {
+				return err
+			}
+
+			defer file.Close()
+
+			_, err = io.Copy(file, reader)
+			if err != nil {
+				return err
+			}
+			return nil
+		}()
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
