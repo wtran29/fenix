4b8c8ad09cafad1c759c509273fe161f5e1af16b
William Tran <wtran29@gmail.com>
2023-06-25 22:34:20 -0700

    setup graceful shutdown for server

--- app/main.go
+++ app/main.go
@@ -4,6 +4,11 @@ import (
 	"myapp/data"
 	"myapp/handlers"
 	"myapp/middleware"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/wtran29/fenix/fenix"
 )
@@ -13,9 +18,32 @@ type application struct {
 	Handlers   *handlers.Handlers
 	Models     data.Models
 	Middleware *middleware.Middleware
+	wg         sync.WaitGroup
 }
 
 func main() {
 	f := initApplication()
-	f.App.ListenAndServe()
+	go f.listenForShutdown()
+	err := f.App.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		f.App.ErrorLog.Println(err)
+	}
+}
+
+func (a *application) shutdown() {
+	// put any clean up tasks here
+
+	// block until the waitgroup is empty
+	a.wg.Wait()
+}
+
+func (a *application) listenForShutdown() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	s := <-quit
+
+	a.App.InfoLog.Println("Received signal", s.String())
+	a.shutdown()
+
+	os.Exit(0)
 }
