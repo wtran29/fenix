0fed7c3edd244f62a8d0e0b2a6006897bab6e687
William Tran <wtran29@gmail.com>
2023-06-25 22:28:59 -0700

    added check for maintenance mode function

--- fenix/middleware.go
+++ fenix/middleware.go
@@ -1,8 +1,10 @@
 package fenix
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/justinas/nosurf"
 )
@@ -27,4 +29,19 @@ func (f *Fenix) NoSurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+func (f *Fenix) CheckForMaintenanceMode(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if maintenanceMode {
+			if !strings.Contains(r.URL.Path, "/public/maintenance.html") {
+				w.WriteHeader(http.StatusServiceUnavailable)
+				w.Header().Set("Retry-After:", "300")
+				w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+				http.ServeFile(w, r, fmt.Sprintf("%s/public/maintenance.html", f.RootPath))
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
 }
